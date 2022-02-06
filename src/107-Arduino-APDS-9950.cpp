d0c3b3434f9012354fc213cc60116ce241cd3aee
Bernhard Mayer <bernhard@generationmake.de>
2022-02-06 14:14:18 +0100

    added functions to source code

--- src/107-Arduino-APDS-9950.cpp
+++ src/107-Arduino-APDS-9950.cpp
@@ -35,28 +35,33 @@ ArduinoAPDS_9950::ArduinoAPDS_9950(APDS_9950::I2cWriteFunc write,
  * PUBLIC MEMBER FUNCTIONS
  **************************************************************************************/
 
-bool ArduinoAPDS_9950::begin(bool const use_extended)
+bool ArduinoAPDS_9950::begin()
 {
   /* Check the CHIP ID if it matches the expected value.
    */
-  if (_io.read(APDS_9950::Register::APDS_9950_ID) != APDS_9950::ID_EXPECTED_ID) {
+  if (_io.read((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_ID)|0x80)) != APDS_9950::ID_EXPECTED_ID) {
     _error = APDS_9950::Error::ChipId;
     return false;
   }
 
-  _io.write(APDS_9950::Register::APDS_9950_WTIME, 0xFF);   /* set WTIME to default value */
-  _io.write(APDS_9950::Register::APDS_9950_ATIME, 0xFF);   /* set ATIME to default value */
-  _io.write(APDS_9950::Register::APDS_9950_ENABLE, 0x0F);   /* set WEN, PEN, AEN and PON bit in ENABLE */
+  _io.write((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_WTIME)|0x80), 0xFF);   /* set WTIME to default value */
+  _io.write((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_ATIME)|0x80), 0xFF);   /* set ATIME to default value */
+  _io.write((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_ENABLE)|0x80), 0x0F);   /* set WEN, PEN, AEN and PON bit in ENABLE */
 
   return true;
 }
 
-
-float ArduinoAPDS_9950::get_lux()
+void ArduinoAPDS_9950::get_colorData(struct colorDataRaw * color)
 {
-  float r=0, light_level=0;
+  color->clear = _io.read16((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_CDATAL)|0xA0));
+  color->red   = _io.read16((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_RDATAL)|0xA0));
+  color->green = _io.read16((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_GDATAL)|0xA0));
+  color->blue  = _io.read16((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_BDATAL)|0xA0));
+}
 
-  return light_level;
+void ArduinoAPDS_9950::get_proximityData(struct proximityDataRaw * proximity)
+{
+  proximity->distance = _io.read16((Register const)((uint8_t)(APDS_9950::Register::APDS_9950_PDATAL)|0xA0));
 }
 
 APDS_9950::Error ArduinoAPDS_9950::error()
